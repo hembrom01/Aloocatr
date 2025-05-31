e56313b3b7d35a6644affe18cf8626e47801ac4d
Ajay Hembrom <Ajay.Hemb123@gmail.com>
2025-05-31 05:39:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:. Error source: - A server/client branch `if (typeof window !== 'undefined')`.
    > - Variable input such as `Date.now()` or `Math.random()` which changes each time it's called.
    > - Date formatting in a user's locale which doesn't match the server.
    > - External changing data without sending a snapshot of it along with the HTML.
    > - Invalid HTML tag nesting.
    >
    > It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
    >
    > See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    >   ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/settings" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <ClientSegmentRoot Component={function RootLayout} slots={{...}} params={{}}>
    >                       <RootLayout params={Promise}>
    >                         <html lang="en" suppressHydrationWarning={true}>
    >                           <body
    >                             className="geist_e531dabc-module__QGiZLq__variable geist_mono_68a01160-module__YLcDdW__var..."
    > -                           data-new-gr-c-s-check-loaded="14.1237.0"
    > -                           data-gr-ext-installed=""
    >                           >
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (45:7) @ RootLayout
    >
    >   43 |   return (
    >   44 |     <html lang="en" suppressHydrationWarning>
    > > 45 |       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
    >      |       ^
    >   46 |         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
    >   47 |           <SidebarProvider>
    >   48 |             <AppSidebar />
    >
    > Call Stack
    > 18
    >
    > Show 16 ignore-listed frame(s)
    > body
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (45:7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning={true}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <SidebarProvider>
             <AppSidebar />
